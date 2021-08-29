595a6e4baafeb51f157a7792becb3bd5b4329ef8
avshir <av.shirinskaya@gmail.com>
2021-08-29 22:05:59 +0300

    feat: add solution, 9 tests passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	// решается на основе стека https://www.youtube.com/watch?v=TqlSlaMak8Y
+	let stack = [];
+	let topElem;
+	
+	for (let i = 0; i < str.length; i++){
+		let currentBracket = str[i];
+		if (bracketsConfig[0][0] === currentBracket) {
+			stack.push(currentBracket);
+		} else {
+			if (stack.length === 0) {
+				return false;
+			}			
+			topElem = stack[stack.length - 1];
+			if (bracketsConfig[0][1] === topElem) {
+				stack.pop()
+			} else {
+				return false;
+			}
+		}
+	}
+	return stack.length === 0;
 }
