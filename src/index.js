b6b5d2e49f43fd333e9484bd5ae3f1c7b5b48343
avshir <av.shirinskaya@gmail.com>
2021-08-30 00:22:49 +0300

    fix: 13 tests passing

--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
 	// решается на основе стека https://www.youtube.com/watch?v=TqlSlaMak8Y
 	let stack = [];
-	let topElem;
+	let openBrackets = [];
+
+	let pairOk = {};
+	for (let i = 0; i < bracketsConfig.length; i++){
+		pairOk[bracketsConfig[i][1]] = bracketsConfig[i][0];
+		openBrackets.push(bracketsConfig[i][0]);
+	}
+	console.log(pairOk);
+	console.log(openBrackets);
 	
 	for (let i = 0; i < str.length; i++){
 		let currentBracket = str[i];
-		if (bracketsConfig[0][0] === currentBracket) {
+
+		// for (let j = 0; j < bracketsConfig.length; j++){
+		if (openBrackets.includes(currentBracket) ) {
 			stack.push(currentBracket);
-		} else {
-			if (stack.length === 0) {
-				return false;
-			}			
-			topElem = stack[stack.length - 1];
-			if (bracketsConfig[0][1] === topElem) {
-				stack.pop()
+			// if (currentBracket === stack[stack.length - 1 - 1] && currentBracket === stack[stack.length - 1] && pairOk[currentBracket] === currentBracket) {
+			// 	stack.pop().pop();
+			// }
+			//console.log(stack, 'this is stack');
 			} else {
-				return false;
+				if (stack.length === 0) {
+					return false;
 			}
-		}
+			
+				let topElem = stack[stack.length - 1];
+				// console.log(topElem);
+				// console.log(currentBracket);
+				// console.log(pairOk[currentBracket]);
+			
+				if (pairOk[currentBracket] === topElem) {
+					stack.pop();
+					//console.log(stack, 'this is stack after pop()');
+				} else {
+					return false;
+				}
+			}
+		// }	
 	}
+
 	return stack.length === 0;
 }
